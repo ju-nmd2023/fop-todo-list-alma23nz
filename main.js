4e6a8008b1a60227864358d32d981e033f73463c
alma23nz <149969050+alma23nz@users.noreply.github.com>
2024-04-12 11:10:35 +0200

    edits

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const taskInput = document.getElementById("inputbox");
-const taskList = document.querySelector(".taskulList");
-
+const taskList = document.getElementById("taskulList");
 function addTask() {
   const text = taskInput.value.trim();
   if (text !== "") {
@@ -8,6 +7,25 @@ function addTask() {
     li.textContent = text;
     taskList.appendChild(li);
     taskInput.value = "";
+
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.addEventListener("change", taskCompleted);
+    li.appendChild(checkbox);
+
+    const deletebtn = document.createElement("button");
+    deletebtn.textContent = "delete";
+    deletebtn.addEventListener("click", deleteTask);
+    li.appendChild(deletebtn);
   }
 }
-function taskCompleted(event) {}
+
+function taskCompleted(event) {
+  const task = event.target;
+  task.classList.toggle("completed");
+}
+
+function deleteTask(event) {
+  const task = event.target.parentElement;
+  taskList.removeChild(task);
+}
