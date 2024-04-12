6c0c7422c3e7cda1b93e2dbde09ffa37df8446c9
alma23nz <149969050+alma23nz@users.noreply.github.com>
2024-04-12 10:25:11 +0200

    edits

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 const taskInput = document.getElementById("inputbox");
+const taskList = document.querySelector(".taskulList");
+
+function addTask() {
+  const text = taskInput.value.trim();
+  if (text !== "") {
+    const li = document.createElement("li");
+    li.textContent = text;
+    taskList.appendChild(li);
+    taskInput.value = "";
+  }
+}
+function taskCompleted(event) {}
