738285d428cefbec5141105d12cc2ab9d3061e7f
alma23nz <149969050+alma23nz@users.noreply.github.com>
2024-04-14 16:23:43 +0200

    edit

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
+//  the following code was addaped from https://dev.to/iamcymentho/implementing-to-do-list-using-javascript-32a7
+
 const taskInput = document.getElementById("inputbox");
 const taskList = document.getElementById("taskulList");
+const itemCountElement = document.getElementById("itemCount");
+
 function addTask() {
   const text = taskInput.value.trim();
   if (text !== "") {
@@ -10,14 +14,17 @@ function addTask() {
 
     const checkbox = document.createElement("input");
     checkbox.type = "checkbox";
+    checkbox.classList.add("task-checkbox");
     checkbox.addEventListener("change", taskCompleted);
     li.appendChild(checkbox);
 
     const deletebtn = document.createElement("button");
     deletebtn.textContent = "delete";
+    deletebtn.classList.add("delete-button");
     deletebtn.addEventListener("click", deleteTask);
     li.appendChild(deletebtn);
   }
+  saveListToLocalstorage();
 }
 
 function taskCompleted(event) {
@@ -28,4 +35,16 @@ function taskCompleted(event) {
 function deleteTask(event) {
   const task = event.target.parentElement;
   taskList.removeChild(task);
+}
+function taskCompleted(event) {
+  const task = event.target.parentElement; // Traverse to the parent <li> element
+  task.classList.toggle("completed");
+}
+function saveListToLocalstorage() {
+  const tasks = [];
+  const items = taskList.getElementsByTagName("li");
+  for (let i = 0; i < items.length; i++) {
+    tasks.push(items[i].textContent);
+  }
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 }
