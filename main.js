e651166a54c3825b3b7f54055e11074bd2670346
alma23nz <149969050+alma23nz@users.noreply.github.com>
2024-04-16 20:12:01 +0200

    edit

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
-// //  the following code was addaped from https://dev.to/iamcymentho/implementing-to-do-list-using-javascript-32a7
+function clickHandlar() {
+  //  the following 22 code line was addaped from https://dev.to/iamcymentho/implementing-to-do-list-using-javascript-32a7
 
-const taskInput = document.getElementById("inputbox");
-const taskList = document.getElementById("taskulList");
-
-function addTask() {
-  const text = taskInput.value.trim();
+  const taskInput = document.getElementById("inputbox");
+  const taskList = document.getElementById("taskulList");
+  const text = taskInput.value;
   if (text !== "") {
     const li = document.createElement("li");
     li.textContent = text;
@@ -22,51 +21,24 @@ function addTask() {
     deletebtn.classList.add("delete-button");
     deletebtn.addEventListener("click", deleteTask);
     li.appendChild(deletebtn);
+    localStorage.list = text;
   }
-  saveListToLocalstorage();
 }
 
 function taskCompleted(event) {
   const task = event.target.parentElement;
   task.classList.toggle("completed");
-  saveListToLocalstorage();
 }
 
 function deleteTask(event) {
   const task = event.target.parentElement;
   taskList.removeChild(task);
-  saveListToLocalstorage();
 }
 
-function saveListToLocalstorage() {
-  const tasks = [];
-  const items = taskList.getElementsByTagName("li");
-  for (let i = 0; i < items.length; i++) {
-    tasks.push(items[i].textContent);
-  }
-  localStorage.setItem("taskList", JSON.stringify(tasks));
+function loadHandlar() {
+  const inputBtn = document.getElementById("addtaskbutton");
+  inputBtn.addEventListener("click", clickHandlar);
+  todoList();
 }
 
-function loadListfromLocalstorage() {
-  const tasks = JSON.parse(localStorage.getItem("taskList"));
-  if (tasks) {
-    tasks.forEach((taskText) => {
-      const li = document.createElement("li");
-      li.textContent = taskText;
-      taskList.appendChild(li);
-
-      const checkbox = document.createElement("input");
-      checkbox.type = "checkbox";
-      checkbox.classList.add("task-checkbox");
-      checkbox.addEventListener("change", taskCompleted);
-      li.appendChild(checkbox);
-
-      const deletebtn = document.createElement("button");
-      deletebtn.textContent = "delete";
-      deletebtn.classList.add("delete-button");
-      deletebtn.addEventListener("click", deleteTask);
-      li.appendChild(deletebtn);
-    });
-  }
-}
-loadListfromLocalstorage();
+window.addEventListener("load", loadHandlar);
